631d8921e39f3dfb3c061637fd350c7c3a6dad14
JustFlyIt <DonRSnyder@gmail.com>
2022-10-17 14:19:44 -0400

    Updated for timing implementation

--- src/encryption/Tester.java
+++ src/encryption/Tester.java
@@ -14,6 +14,10 @@ public class Tester {
         double encryptedMessageLength = 0;
         double  decryptedMessageLength = 0;
         double percentageEncryptedOverhead = 0;
+        long encryptTime = 0;
+        long decryptTime = 0;
+        long initialTime = 0;
+        
         AES aes;
         NumberFormat defaultFormat = NumberFormat.getPercentInstance();
         
@@ -25,8 +29,13 @@ public class Tester {
 					System.out.println("Simple Text Encryption Example...");
 					System.out.println("Test text: " + input);
 
+					initialTime = System.nanoTime();
 					encryptedMessage = aes.encrypt(input);
+					encryptTime = (System.nanoTime() - initialTime)/1000000;
+
+					initialTime = System.nanoTime();
 		            decryptedMessage = aes.decrypt(encryptedMessage);
+					decryptTime = (System.nanoTime() - initialTime)/1000000;
 		            
 		            encryptedMessageLength = encryptedMessage.length();
 		            decryptedMessageLength = decryptedMessage.length();
@@ -34,9 +43,10 @@ public class Tester {
 
 		            System.out.println("Encrypted Message: " + encryptedMessage);
 		            System.out.println("Encrypted Message - number of bytes: " + encryptedMessageLength);
+		            System.out.println("Time to encrypt (ms): " + encryptTime);
 		            System.out.println("Decrypted Message: " + decryptedMessage);
 		            System.out.println("Decrypted Message - number of bytes: " + decryptedMessageLength);
-		            
+		            System.out.println("Time to decrypt (ms): " + decryptTime);
 		    		defaultFormat.setMinimumFractionDigits(2);
 		            System.out.println("Encryption Overhead Percentage: " + defaultFormat.format(percentageEncryptedOverhead));
 
@@ -45,15 +55,26 @@ public class Tester {
 					System.out.println("Text File Encryption Example...");
 					System.out.println("Test Input file: " + input);
 
+					initialTime = System.nanoTime();
 					encryptedMessage = aes.encrypt(FileAsStringReader.readFileAsString(input));
+					encryptTime = (System.nanoTime() - initialTime)/1000000;
+
+					initialTime = System.nanoTime();
 		            decryptedMessage = aes.decrypt(encryptedMessage);
+					decryptTime = (System.nanoTime() - initialTime)/1000000;
 		            
 		            encryptedMessageLength = encryptedMessage.length();
 		            decryptedMessageLength = decryptedMessage.length();
 		            percentageEncryptedOverhead = (float) ((encryptedMessageLength - decryptedMessageLength)/encryptedMessageLength);
 
+		            encryptedMessageLength = encryptedMessage.length();
+		            decryptedMessageLength = decryptedMessage.length();
+		            percentageEncryptedOverhead = (float) ((encryptedMessageLength - decryptedMessageLength)/encryptedMessageLength);
+
 		            System.out.println("Encrypted Message - number of bytes: " + encryptedMessage.length());
+		            System.out.println("Time to encrypt (ms): " + encryptTime);
 		            System.out.println("Decrypted Message - number of bytes: " + decryptedMessage.length());
+		            System.out.println("Time to decrypt (ms): " + decryptTime);
 		            
 		    		defaultFormat.setMinimumFractionDigits(2);
 		            System.out.println("Encryption Overhead Percentage: " + defaultFormat.format(percentageEncryptedOverhead));
