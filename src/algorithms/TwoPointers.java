95fe9b8cdc5f4a0eeb4c595db82add8e66e4e6cd
JustFlyIt <DonRSnyder@gmail.com>
2022-11-17 14:49:02 -0500

    Added Removed Duplicates I and II along with Container With Most Water

--- src/algorithms/TwoPointers.java
+++ src/algorithms/TwoPointers.java
@@ -57,91 +57,87 @@ public class TwoPointers {
 	}
 	
 	public static int removeDuplicatesFromSortedArray(int[] inputNumbers) {
-		// O(n)
-		/* 	Constraints
-		 * 	1 <= nums.length <= 3 * 104
-		 * -100 <= inputNumbers[i] <= 100
-		 * inputNumbers is sorted in non-decreasing order.
-		*/
-		
 		int L = 0;
-		int R = 0;
 		int numberOfElements = inputNumbers.length - 1;
-		int numberUnique = 1;
 		
-		while (true) {
-			if (!(inputNumbers[L] == inputNumbers[R])) {
-				System.out.print(inputNumbers[L] + "  ");
+		for (int R=0; R <= numberOfElements; R++) {
+			if (inputNumbers[L] != inputNumbers[R])
 				inputNumbers[++L] = inputNumbers[R];
-				numberUnique++;
-			}
-			
-			R++;
-			
-			if (R > numberOfElements) { 
-				// Print the last unique value
-				System.out.print(inputNumbers[L] + "  ");
-				while (L < numberOfElements) {
-					inputNumbers[++L] = '_';
-					System.out.print("_ ");
-				}
-			
-				break;
-			}
 		}
-		
-		return numberUnique;
-	}
-	
-	public static int removeDuplicatesFromSortedArrayII(int[] inputNumbers) {
-		// O(n)
-		/* 	Constraints
-		 * 	1 <= nums.length <= 3 * 104
-		 * -100 <= inputNumbers[i] <= 100
-		 * inputNumbers is sorted in non-decreasing order.
-		*/
-		
-		int L = 0;
-		int R = 0;
-		int numberOfElements = inputNumbers.length;
-		int numberUnique = 0;
-		int twoTries = 2;
-		
-		while (true) {
-			if (inputNumbers[L] == inputNumbers[R]) {
-				if (twoTries != 0) {
-					numberUnique++;
-					twoTries--;
-					L++;
-				} else {
-					twoTries = 2;
-					L = R;
-				}
-			} else {
-				inputNumbers[L] = inputNumbers[R];
-				L++;
-				numberUnique++;
-				twoTries--;
-			}
-			
-			R++;
-			
-			if (R == numberOfElements) { 
-				while (L <= numberOfElements-1) {
-					inputNumbers[L++] = -1;
-				}
-			
-				break;
-			}
-		}
+
+		int numberUnique = L+1;
+
+		while (L < numberOfElements) 
+			inputNumbers[++L] = -1;
 		
 		PrintSupport.printArray(inputNumbers);
 		
 		return numberUnique;
 	}
 	
+	public static int removeDuplicatesFromSortedArrayII(int[] inputNumbers) {
+	    int numberUnique = inputNumbers.length;
+        int L = 2;
+		
+		if (inputNumbers.length >= 3) {
+	        for (int R = 2; R < inputNumbers.length; R++) {
+	            if (inputNumbers[R] != inputNumbers[L-2])
+	            	inputNumbers[L++] = inputNumbers[R];
+	        }
+		}
+
+		numberUnique = L;
+
+		while (L < inputNumbers.length) 
+			inputNumbers[L++] = -1;
+			
+		PrintSupport.printArray(inputNumbers);
+
+		return numberUnique;		
+	}
+	
+	public static int ContainerWithMostWaterSlow(int[] height) {
+        int currentVolume = 0; 
+        int maxVolume = 0;
+        int currentSide = 0;
+        int length = height.length - 1;
+		
+        for (int R = length; R >= 0; R--) {
+        	for (int L = 0; L < height.length; L++) {
+	        	if (height[L] < height[R]) 
+	        		currentSide = height[L];
+	        	else 
+	            	currentSide = height[R];
+	        	
+	        	currentVolume = currentSide * (R - L);
+	        	if (currentVolume > maxVolume)
+	        		maxVolume = currentVolume;
+        	}
+		}
+
+		return maxVolume;		
+	}
+	
+	public static int ContainerWithMostWaterFast(int[] height) {
+		// O(n)
+        int maxVolume = 0;
+		int R = height.length - 1;
+		int L = 0;
+		 
+		while (L != R) {
+	        	maxVolume = Math.max(maxVolume, Math.min(height[L], height[R]) * (R - L));
+	        	
+	        	if (height[L] <= height[R])
+	        		L++;
+	        	else
+	        		R--;
+		}
+
+		return maxVolume;		
+	}
+
 	public static void main(String[] args) {
-		int x = 4;
+		int x = 5;
 		
 		switch(x) {
 			case 1:
@@ -168,14 +164,25 @@ public class TwoPointers {
 				System.out.println("\nNumber unique: " + numberUnique);
 				break;
 			case 4:
-				//int inputWithDuplicatesII[] = {0, 0, 0, 1, 1, 1, 2, 3, 3};
-				int inputWithDuplicatesII[] = {1, 1, 1};
+				//int inputWithDuplicatesII[] = {0, 0, 0, 0, 0, 1, 1, 1, 2, 3, 3};
+				int inputWithDuplicatesII[] = {1, 1, 1, 2, 2, 3};
+				//int inputWithDuplicatesII[] = {0, 0, 1, 1, 1, 1, 2, 3 ,3};
+				//int inputWithDuplicatesII[] = {1, 1, 1};
 				//int inputWithDuplicatesII[] = {1, 2, 2};
 				System.out.print("Input - ");
 				PrintSupport.printArray(inputWithDuplicatesII);
 				System.out.print("String with duplicates (II) removed: ");
 				int numberUniqueII = removeDuplicatesFromSortedArrayII(inputWithDuplicatesII);
 				System.out.println("\nNumber unique II: " + numberUniqueII);
+				break;
+			case 5:
+				int ratings[] = {1, 1};
+				//int ratings[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+				//int ratings[] = {1, 8, 6, 112, 5, 100, 3, 8, 7};
+				System.out.print("Ratings Input - ");
+				PrintSupport.printArray(ratings);
+				int volume = ContainerWithMostWaterFast(ratings);
+				System.out.println("\nVolume: " + volume);
 				break;
 			default:
 				System.out.println("Valid Solution Not Specified");
